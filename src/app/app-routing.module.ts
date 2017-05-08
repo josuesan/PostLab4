b0bbce31e8d3ffaa811c2876e107f677a914b414
josuesan <josueguitar2@gmail.com>
2017-05-08 16:33:58 -0400

    formularios validados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule }              from '@angular/core';
 import { RouterModule, Routes }  from '@angular/router';
 import { LoginComponent }   from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ProductsList }  from './products-list.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'productos', component: ProductsList }
 ];
 @NgModule({
   imports: [
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Http, Headers} from '@angular/http';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+	public myForm: FormGroup; 
+  	constructor(public fb: FormBuilder, public http: Http) { 
+  		
+  		this.myForm = this.fb.group({
+	  	mail: ["",Validators.compose([Validators.required,Validators.email ])],
+	  	password: ["",Validators.compose([Validators.required,Validators.minLength(5)])]
+  		});
 
-  ngOnInit() {
+  	}
+
+  	ngOnInit() {
+  	}
+
+  log () {
+  	let formData = this.myForm.value;
+  	console.log(formData);
+  	var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+
+  	/*this.http.post('http://localhost:5000/login', JSON.stringify(formData),{ headers: headers })      
+  	.subscribe(data => {
+            alert(data.json().success);
+            this.PL.refresh();
+           
+      }, error => {
+          console.log(error.json());
+      });*/
+
   }
-
 }
