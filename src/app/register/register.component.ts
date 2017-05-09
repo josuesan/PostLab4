4b801e7bb7b504f3a3b703c49e402179ae52ebea
josuesan <josueguitar2@gmail.com>
2017-05-08 23:59:17 -0400

    Registrando

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Http, Headers} from '@angular/http';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public myForm: FormGroup; 
 
-  ngOnInit() {
+  	constructor(public fb: FormBuilder, public http: Http) { 
+  		
+  		this.myForm = this.fb.group({
+      username: ["",Validators.required],
+	  	email: ["",Validators.compose([Validators.required,Validators.email ])],
+	  	password: ["",Validators.compose([Validators.required,Validators.minLength(6)])],
+      name: ["",Validators.required],
+      lastname: ["",Validators.required],
+      birthdate: ["",Validators.required],
+      gender: ["",Validators.required]
+  		});
+  	}
+
+  	ngOnInit() {
+  	}
+
+  register () {
+  	let formData = this.myForm.value;
+  	console.log(formData);
+  	var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    headers.append('CrossDomain', true);
+    this.http.post('http://localhost:5000/registro', JSON.stringify(formData),{ headers: headers })      
+  	.subscribe(data => {
+            alert(data.json().success);
+         
+      }, error => {
+          console.log(error.json());
+      });
+
   }
 
 }
